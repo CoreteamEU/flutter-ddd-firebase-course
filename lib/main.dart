93f05e6696ad916275e33ca90da05404b0d7d0e8
Juris Andersons <juris.andersons@emergen.com>
2020-11-25 12:02:00 +0200

    test commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:injectable/injectable.dart';
 import 'package:notes_firebase_ddd_course/injection.dart';
 import 'package:notes_firebase_ddd_course/presentation/core/app_widget.dart';
